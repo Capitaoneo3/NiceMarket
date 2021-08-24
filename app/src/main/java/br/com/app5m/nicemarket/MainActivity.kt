d5329f9a46b5462e05f9ced997a87a6001750b29
joao <joaocapitaneo@gmail.com>
2021-08-24 20:38:27 -0300

    Initial commit

---
+++ app/src/main/java/br/com/app5m/nicemarket/MainActivity.kt
@@ -0,0 +1,11 @@
+package br.com.app5m.nicemarket
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
